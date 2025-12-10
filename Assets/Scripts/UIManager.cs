503a0b370423f0079cbff82dae2a252406e07967
MohitKumarSinghIndia <Mohitkumarsingh.india@gmail.com>
2025-12-11 02:15:08 +0530

    Fixed Game UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,48 +6,98 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
 
-    public TextMeshProUGUI player1CountText;
-    public TextMeshProUGUI player2CountText;
-    public TextMeshProUGUI currentTurnText;
-    public TextMeshProUGUI winMessageText;
-    public GameObject winPanel;
-    public Button restartButton;
-    public GameManager gameManager;
+    [Header("UI References")]
+    [SerializeField] private TextMeshProUGUI player1CountText;
+    [SerializeField] private TextMeshProUGUI player2CountText;
+    [SerializeField] private TextMeshProUGUI winMessageText;
 
-    void Awake()
+    [SerializeField] private GameObject player1TurnImage;
+    [SerializeField] private GameObject player2TurnImage;
+
+    [SerializeField] private RectTransform player1Holder;
+    [SerializeField] private RectTransform player2Holder;
+
+    [SerializeField] private RectTransform winPanel;
+    [SerializeField] private Button restartButton;
+
+    private bool isGameOver;
+
+    private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Instance = this;
     }
 
     private void OnEnable()
     {
-        restartButton.onClick.AddListener(gameManager.OnRestartButtonClicked);
+        if (restartButton != null)
+            restartButton.onClick.AddListener(OnRestartClicked);
     }
 
     private void OnDisable()
     {
-        restartButton.onClick.RemoveListener(gameManager.OnRestartButtonClicked);
+        if (restartButton != null)
+            restartButton.onClick.RemoveListener(OnRestartClicked);
     }
 
     public void InitializeUI()
     {
-        winPanel.SetActive(false);
+        winPanel.gameObject.SetActive(false);
         restartButton.gameObject.SetActive(false);
+        isGameOver = false;
         winMessageText.text = "";
         UpdateUI();
     }
 
     public void UpdateUI()
     {
-        player1CountText.text = "P1 Magnets: " + GameManager.Instance.player1Magnets;
-        player2CountText.text = "P2 Magnets: " + GameManager.Instance.player2Magnets;
-        currentTurnText.text = "Turn: " + GameManager.Instance.currentTurn.ToString();
+        player1CountText.text = $"P1 Magnets: {GameManager.Instance.player1Magnets}";
+        player2CountText.text = $"P2 Magnets: {GameManager.Instance.player2Magnets}";
+
+        UpdateTurnIndicator(GameManager.Instance.currentTurn);
+    }
+
+    private void UpdateTurnIndicator(PlayerTurn turn)
+    {
+        if (isGameOver) return;
+
+        bool isP1 = turn == PlayerTurn.Player1;
+
+        player1TurnImage.SetActive(isP1);
+        player2TurnImage.SetActive(!isP1);
+
+        if (isP1)
+        {
+            player1Holder.SetAsLastSibling();
+        }
+        else
+        {
+            player2Holder.SetAsLastSibling();
+        }
     }
 
     public void ShowWin(PlayerTurn winner)
     {
-        winMessageText.text = winner + " Wins!";
-        winPanel.SetActive(true);
+        isGameOver = true;
+
+        winPanel.SetAsLastSibling();
+        winPanel.gameObject.SetActive(true);
+
         restartButton.gameObject.SetActive(true);
+
+        winMessageText.text = $"{winner} Wins!";
+
+        player1TurnImage.SetActive(false);
+        player2TurnImage.SetActive(false);
+    }
+
+    private void OnRestartClicked()
+    {
+        GameManager.Instance.OnRestartButtonClicked();
     }
 }
